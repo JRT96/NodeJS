2139aaa1f9e9c7d267b426c6b565adc2431f47f3
Jose <t.jose51@gmail.com>
2023-04-24 13:14:35 -0400

    added code to static server file

---
+++ StaticFiles/server.js
@@ -0,0 +1,31 @@
+  const fs = require('fs');
+  const http = require('http');
+
+  const server = http.createServer((req, res) => {
+    console.log(req.url.split("?")[0]);
+    console.log(req.url.split("?")[1]);
+    if(req.url === '/'){
+        serveResource('index.html', res);
+    } 
+    else if (req.url === '/about'){
+        serveResource('about.html', res);
+    }
+    else {
+        res.writeHeader(404, {'Content-Type': 'text/html'});
+         res.end('<h1>404 Not Found</h1>');
+    
+    };
+    
+});
+
+server.listen(3000, () => {
+    console.log('Server running at http://localhost:3000/');
+});
+
+const serveResource = (resource, res) => {
+    fs.readFile(resource, (err, data) => {
+        if (err) throw err;
+        res.writeHeader(200, {'Content-Type': 'text/html'});
+         res.end(data);
+    });
+} 
