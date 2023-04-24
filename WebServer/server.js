033dbe2aa06fe20688f8c5cccd7f3c8ec37e264c
Jose <t.jose51@gmail.com>
2023-04-24 10:56:56 -0400

    updated server file

--- WebServer/server.js
+++ WebServer/server.js
@@ -2,7 +2,8 @@ const http =  require('http');
 const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    console.log(req.url)
+    console.log(req.url.split("?")[0]);
+    console.log(req.url.split("?")[1]);
     if(req.url === '/'){
         serveResource('index.html', res);
     } 
