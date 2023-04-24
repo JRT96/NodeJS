a3ff23e005c3b352af66e36b72192b05aa17cb18
Jose <t.jose51@gmail.com>
2023-04-24 10:38:31 -0400

    web server with node and html

--- WebServer/server.js
+++ WebServer/server.js
@@ -1,11 +1,34 @@
 const http =  require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
+    console.log(req.url)
+    if(req.url === '/'){
+        serveResource('index.html', res);
+    } 
+    else if (req.url === '/about'){
+        serveResource('about.html', res);
+    }
+    else {
+        res.writeHeader(404, {'Content-Type': 'text/html'});
+         res.end('<h1>404 Not Found</h1>');
+    
+    };
+        
+
+    
+    
 });
 
 server.listen(3000, () => {
     console.log('Server running at http://localhost:3000/');
 });
+
+const serveResource = (resource, res) => {
+    fs.readFile(resource, (err, data) => {
+        if (err) throw err;
+        res.writeHeader(200, {'Content-Type': 'text/html'});
+         res.end(data);
+    });
+} 
+
