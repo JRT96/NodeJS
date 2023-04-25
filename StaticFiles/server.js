a654b38112ff688cf282c63430c2f848bcb4ce59
Jose <t.jose51@gmail.com>
2023-04-25 09:37:35 -0400

    added about html and trying to fix server file

--- StaticFiles/server.js
+++ StaticFiles/server.js
@@ -21,9 +21,9 @@ const server = http.createServer((req, res) => {
                 }
             });
         }}
-);
+    );
+});
 
 server.listen(3000, () => {
     console.log('Server running at http://localhost:3000/');
-});
 });
