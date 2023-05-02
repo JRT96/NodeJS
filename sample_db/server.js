090f56d9d9003fb37aceb595dd209d2741a5db77
Jose <t.jose51@gmail.com>
2023-05-02 09:14:09 -0400

    starting over. Going to add static and dynamic files

--- sample_db/server.js
+++ sample_db/server.js
@@ -1,12 +1,4 @@
 const mysql = require('mysql');
-const toSqlDatetime = (inputDate) => {
-    const date = new Date(inputDate)
-    const dateWithOffest = new Date(date.getTime() - (date.getTimezoneOffset() * 60000))
-    return dateWithOffest
-        .toISOString()
-        .slice(0, 19)
-        .replace('T', ' ')
-}
 
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -18,19 +10,5 @@ const connection = mysql.createConnection({
 connection.connect((err) => {
     if (err) throw err;
     console.log('Connected to MySQL database!');
-});
-
-
-const sql = "INSERT INTO users (name, email, created_at, updated_at) VALUES = ?";
-const values = [
-    ['John Merner', 'john@sample.com', toSqlDatetime(new Date()), toSqlDatetime(new Date())],
-    ['Kelly Morrison', 'kelly@sample.com', toSqlDatetime(new Date()), toSqlDatetime(new Date())],
-    ['Jennifer Smith', 'jennifer@sample.com', toSqlDatetime(new Date()), toSqlDatetime(new Date())],
-    ['Michael Jordan', 'michael@sample.com', toSqlDatetime(new Date()), toSqlDatetime(new Date())]
-];
-
-connection.query(sql, [values], (err, result) => {
-    if (err) throw err;
-    console.log(`${result.affectedRows} record(s) inserted`);
 });
 
