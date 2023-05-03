8c448f998985c5a4dcdca547792b70c4ed4f8712
Jose <t.jose51@gmail.com>
2023-05-03 13:34:48 -0400

    added missing checbox to index and added create server module to server file

--- sample_db/server.js
+++ sample_db/server.js
@@ -1,4 +1,8 @@
 const mysql = require('mysql');
+const fs = require('fs');
+const http = require('http');
+const path = require('path');
+// const { url } = require('inspector');
 
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -11,4 +15,35 @@ connection.connect((err) => {
     if (err) throw err;
     console.log('Connected to MySQL database!');
 });
+
+
+
+server.listen(3000, () => {
+    console.log('Server running at http://localhost:3000/');
+  });
+
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  const filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url);
+  fs.access(filePath, fs.constants.F_OK, (err) => {
+      if(err) {
+          res.statusCode = 404;
+          res.end('404 - Page  not found');
+      } else {
+          fs.readFile(filePath, (err, data) => {
+              if(err) {
+                  res.statusCode = 500;
+                  res.end('500 - Internal server error');
+              } else {
+                  res.statusCode = 200;
+                  res.end(data);
+              }
+          });
+      }}
+  );
+});
+
+
+
 
