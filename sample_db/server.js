8c86028ad62b1bf695854490d9c9c9c04836c753
Jose <t.jose51@gmail.com>
2023-05-04 10:30:18 -0400

    updated button and form on index page added code to server file to add data to database

--- sample_db/server.js
+++ sample_db/server.js
@@ -2,7 +2,6 @@ const mysql = require('mysql');
 const fs = require('fs');
 const http = require('http');
 const path = require('path');
-// const { url } = require('inspector');
 
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -18,14 +17,13 @@ connection.connect((err) => {
 
 
 
-server.listen(3000, () => {
-    console.log('Server running at http://localhost:3000/');
-  });
-
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/html'});
   const filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url);
+  if (req.url === '/add'){
+    addPerson(req, res);
+  }
   fs.access(filePath, fs.constants.F_OK, (err) => {
       if(err) {
           res.statusCode = 404;
@@ -44,6 +42,28 @@ const server = http.createServer((req, res) => {
   );
 });
 
+server.listen(3000, () => {
+    console.log('Server running at http://localhost:3000/');
+  });
 
+let addPerson = (req, res) => {
+    let body = '';
+  req.on('data', (chunk) => {
+    body += chunk.toString();
+  });
+  req.on('end', () => {
+    const data = new URLSearchParams(body);
+    const person = Object.fromEntries(data);
+    console.log(person);
+    connection.query('INSERT INTO users SET ?', person, (err, result) => {
+      if (err) throw err;
+      console.log('Person added to database.');
+      // res.writeHead(302, { 'location': '/add' }); // Redirect to add product page
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write("Person added to database")
+            res.end();
+    });
+  });
+};
 
 
