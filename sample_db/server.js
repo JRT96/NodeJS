8416aeb4dbbf3593750cde6ccadfa470abb6be99
rolorh <rolorh@gmail.com>
2023-05-04 11:39:47 -0400

    Updated location

--- sample_db/server.js
+++ sample_db/server.js
@@ -1,53 +1,53 @@
-const mysql = require('mysql');
+const mysql = require('mysql2');
 const fs = require('fs');
 const http = require('http');
 const path = require('path');
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root244261#',
-    database: 'sample_db'
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  database: 'sample_db'
 });
 
 connection.connect((err) => {
-    if (err) throw err;
-    console.log('Connected to MySQL database!');
+  if (err) throw err;
+  console.log('Connected to MySQL database!');
 });
 
-
-
-
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.writeHead(200, { 'Content-Type': 'text/html' });
   const filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url);
-  if (req.url === '/add'){
+  if (req.url === '/add') {
     addPerson(req, res);
   }
-  fs.access(filePath, fs.constants.F_OK, (err) => {
-      if(err) {
-          res.statusCode = 404;
-          res.end('404 - Page  not found');
+  else {
+    fs.access(filePath, fs.constants.F_OK, (err) => {
+      if (err) {
+        res.statusCode = 404;
+        res.end('404 - Page  not found');
       } else {
-          fs.readFile(filePath, (err, data) => {
-              if(err) {
-                  res.statusCode = 500;
-                  res.end('500 - Internal server error');
-              } else {
-                  res.statusCode = 200;
-                  res.end(data);
-              }
-          });
-      }}
-  );
+        fs.readFile(filePath, (err, data) => {
+          if (err) {
+            res.statusCode = 500;
+            res.end('500 - Internal server error');
+          } else {
+            res.statusCode = 200;
+            res.end(data);
+          }
+        });
+      }
+    }
+    );
+  }
 });
 
 server.listen(3000, () => {
-    console.log('Server running at http://localhost:3000/');
-  });
+  console.log('Server running at http://localhost:3000/');
+});
 
 let addPerson = (req, res) => {
-    let body = '';
+  let body = '';
   req.on('data', (chunk) => {
     body += chunk.toString();
   });
@@ -59,9 +59,8 @@ let addPerson = (req, res) => {
       if (err) throw err;
       console.log('Person added to database.');
       // res.writeHead(302, { 'location': '/add' }); // Redirect to add product page
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write("Person added to database")
-            res.end();
+      res.writeHead(302, { location: '/' });
+      res.end();
     });
   });
 };
